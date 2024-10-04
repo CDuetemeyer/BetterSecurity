c3ff0d78aa2304acd37249cb979bcc939b7c9451
Christian Duetemeyer <christian@novartech.net>
2024-10-04 14:35:26 -0400

    Update VersionUtils.java

--- src/main/java/it/zs0bye/bettersecurity/bukkit/utils/VersionUtils.java
+++ src/main/java/it/zs0bye/bettersecurity/bukkit/utils/VersionUtils.java
@@ -22,7 +22,7 @@ import org.bukkit.Bukkit;
 public class VersionUtils {
 
     public static double getVersion() {
-        final String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[2];
+        final String version = "1_21";
         final String[] parts = version.split("_");
         return Double.parseDouble(parts[0].substring(1) + "." + parts[1]);
     }
@@ -35,7 +35,7 @@ public class VersionUtils {
     }
 
     public static boolean legacy() {
-        return checkVersion(1.8, 1.9, 1.10, 1.11, 1.12);
+        return false;
     }
 
 }
